bd140454480bdd6405d6c05ba3dac0cc6ea355af
지의신 <64007655+usgeeus@users.noreply.github.com>
2024-09-24 15:44:39 +0900

    delete onlyOwner in MockTon

--- test/shared/MockTON.sol
+++ test/shared/MockTON.sol
@@ -4,11 +4,13 @@ pragma solidity ^0.8.26;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-
 contract MockTON is ERC20, Ownable {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(msg.sender) {}
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {}
 
-    function mint(address account, uint256 amount) external onlyOwner{
+    function mint(address account, uint256 amount) external {
         _mint(account, amount);
     }
 }
